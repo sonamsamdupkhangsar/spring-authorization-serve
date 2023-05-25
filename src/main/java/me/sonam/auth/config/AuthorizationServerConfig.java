a6aaa563839ac81c20e60ad04ce8c0bb61c3c17b
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2023-05-25 11:59:29 -0600

    gradle build test failing

--- src/main/java/me/sonam/auth/config/AuthorizationServerConfig.java
+++ src/main/java/me/sonam/auth/config/AuthorizationServerConfig.java
@@ -103,98 +103,6 @@ public class AuthorizationServerConfig {
         return new HttpSessionRequestCache();
     }
 
-  //  @PostConstruct
-    private void savePublicRegisteredClient() {
-        final String clientId = "public-client";
-        Optional<Client> cLientOptional = clientRepository.findByClientId(clientId);
-        cLientOptional.ifPresent(client -> clientRepository.delete(client));
-
-        RegisteredClient registeredClient = jpaRegisteredClientRepository.findByClientId(clientId);
-        if (registeredClient != null) {
-            LOG.info("registered public client exists");
-        }
-        else {
-            registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                    .clientId(clientId)
-                    .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
-                    .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                    //.redirectUri("http://localhost:8080")
-                   // .redirectUri("http://127.0.0.1:8080/login/oauth2/code/pkce")
-                    .redirectUri("http://127.0.0.1:8080")
-                    .scope(OidcScopes.OPENID)
-                    .scope(OidcScopes.PROFILE)
-                    .scope("message.read")
-                    .scope("message.write")
-                    .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true)
-                            .requireProofKey(true).build())
-                    .build();
-            jpaRegisteredClientRepository.save(registeredClient);
-
-            LOG.info("saved registeredClient");
-        }
-    }
-
-    @PostConstruct
-    private void savePrivateRegisteredClient() {
-        final String clientId = "private-client";
-       // Optional<Client> cLientOptional = clientRepository.findByClientId(clientId);
-       // cLientOptional.ifPresent(client -> clientRepository.delete(client));
-
-        RegisteredClient registeredClient = jpaRegisteredClientRepository.findByClientId(clientId);
-        if (registeredClient != null) {
-            LOG.info("registered private client exists");
-        }
-        else {
-            registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                    .clientId(clientId)
-                    .clientSecret("{noop}secret")
-                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                    .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                    .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                    .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                    .redirectUri("http://127.0.0.1:8080/login/oauth2/code/pkce")
-                    .redirectUri("http://127.0.0.1:8080/authorized")
-                    //.postLogoutRedirectUri("http://127.0.0.1:8080/logged-out")
-                    .scope(OidcScopes.OPENID)
-                    .scope(OidcScopes.PROFILE)
-                    .scope(OidcScopes.EMAIL)
-                    .scope("message.read")
-                    .scope("message.write")
-                    .clientSettings(ClientSettings.builder().requireAuthorizationConsent(false)
-                            .requireProofKey(true).build())
-                    .build();
-            jpaRegisteredClientRepository.save(registeredClient);
-
-            LOG.info("saved registeredClient");
-        }
-    }
-    //@PostConstruct
-    private void saveClientCredential() {
-        final String clientId = "oauth-client";
-        clientRepository.deleteAll();
-
-        RegisteredClient registeredClient = jpaRegisteredClientRepository.findByClientId(clientId);
-        if (registeredClient != null) {
-            LOG.info("registered client exists");
-        }
-        else {
-            registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                    .clientId(clientId)
-                    .clientSecret("{noop}oauth-secret")
-                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                    .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                    .scope(OidcScopes.OPENID)
-                    .scope(OidcScopes.PROFILE)
-                    .scope(OidcScopes.EMAIL)
-                    .scope("message.read")
-                    .scope("message.write")
-                    .build();
-            jpaRegisteredClientRepository.save(registeredClient);
-
-            LOG.info("save a client-credential");
-        }
-    }
-
     @Bean
     public JWKSource<SecurityContext> jwkSource() {
         RSAKey rsaKey = Jwks.generateRsa();
@@ -212,38 +120,13 @@ public class AuthorizationServerConfig {
         return AuthorizationServerSettings.builder().build();
     }
 
-    @Bean
+/*    @Bean
     public OAuth2TokenCustomizer<JwtEncodingContext> auth2TokenCustomizer() {
         LOG.info("get authorities");
         return context -> {
             context.getClaims().claim("authorities", context.getAuthorizedScopes());
         };
-    }
+    }*/
 
-    @PostConstruct
-    public void registeredClientRepository() {
-        //clientRepository.deleteAll();
 
-        if (clientRepository.findByClientId("messaging-client") != null) {
-            LOG.info("message-client already exists");
-        }
-        else {
-            RegisteredClient confidentialClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                    .clientId("messaging-client")
-                    .clientSecret("{noop}secret")
-                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                    .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                    .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                    .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                    .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
-                    .redirectUri("http://127.0.0.1:8080/authorized")
-                    .scope(OidcScopes.OPENID)
-                    .scope(OidcScopes.PROFILE)
-                    .scope("message.read")
-                    .scope("message.write")
-                    .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).requireProofKey(true).build())
-                    .build();
-            jpaRegisteredClientRepository.save(confidentialClient);
-        }
-    }
 }
