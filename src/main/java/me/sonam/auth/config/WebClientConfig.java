5bde4ac11a683f46667eb366d27bff4dd8558cde
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2023-09-21 09:21:14 -0600

    use issuer url with /issuer

--- src/main/java/me/sonam/auth/config/WebClientConfig.java
+++ src/main/java/me/sonam/auth/config/WebClientConfig.java
@@ -2,6 +2,7 @@ package me.sonam.auth.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
