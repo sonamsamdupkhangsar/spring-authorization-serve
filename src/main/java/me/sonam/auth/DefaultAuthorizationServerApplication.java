d5a6a45ef3067e73679d03dd512aa831449df2ba
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2023-10-01 21:27:54 -0600

    update

--- src/main/java/me/sonam/auth/DefaultAuthorizationServerApplication.java
+++ src/main/java/me/sonam/auth/DefaultAuthorizationServerApplication.java
@@ -19,7 +19,9 @@ package me.sonam.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"me.sonam.auth", "me.sonam.security"})
 public class DefaultAuthorizationServerApplication {
 
